6f58cbcdf7f2bf52ed0def69379001da7ba9a67e
Akshat Alurkar <akshat@MacBookPro.lan>
2026-04-08 03:20:25 -0700

    Added and tested SQLite database with User model

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 
+
 load_dotenv()
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    oauth_token = db.Column(db.Text, nullable=True)
+    refresh_token = db.Column(db.Text, nullable=True)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
